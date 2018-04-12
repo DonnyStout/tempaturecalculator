d0a4a88f5ff5211c257eaa0f792e845eaf623a3b
DonnyStout <donnystout47@gmail.com>
2018-04-12 13:44:53 -0600

    fixed the onclicklistener and fixed the calculations

--- app/src/main/java/edu/cnm/deepdive/tempaturecalculator/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/tempaturecalculator/MainActivity.java
@@ -23,49 +23,42 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
     setContentView(R.layout.activity_main);
     fahr = findViewById(R.id.fahrenheit_input);
     fahrButton = findViewById(R.id.fahrenheit_button);
+    fahrButton.setOnClickListener(this);
     cels = findViewById(R.id.celsius_input);
     celsButton = findViewById(R.id.celsius_button);
+    celsButton.setOnClickListener(this);
     kelv = findViewById(R.id.kelvin_input);
     kelvButton = findViewById(R.id.kelvin_button);
+    kelvButton.setOnClickListener(this);
   }
 
   @Override
   public void onClick(View v) {
     if (v.getId() == fahrButton.getId()) {
-      clear();
       setFahr();
     }
     else if (v.getId() == celsButton.getId()) {
-      clear();
       setCels();
     }
     else if (v.getId() == kelvButton.getId()) {
-      clear();
       setKelv();
     }
   }
 
-  private void clear() {
-    fahr.setText("");
-    cels.setText("");
-    kelv.setText("");
-  }
-
   private void setFahr() {
-    String fahrenheit = ((EditText) findViewById(R.id.fahrenheit_input)).getText().toString();
-    int fahrenheitValue = Integer.parseInt(fahrenheit);
-    int celsiusConvert = (fahrenheitValue - 32) * 9 / 5;
-    cels.setText(getString(R.string.int_place_holder, celsiusConvert));
+    String fahrenheit = fahr.getText().toString();
+    double fahrenheitValue = Double.parseDouble(fahrenheit);
+    double celsiusConvert = (fahrenheitValue - 32) * 5 / 9;
+    cels.setText(getString(R.string.double_place_holder, celsiusConvert));
     double kelvinConvert = (fahrenheitValue + 459.67) * 5 / 9;
     kelv.setText(getString(R.string.double_place_holder, kelvinConvert));
   }
 
   private void setCels() {
-    clear();
     String celsius = cels.getText().toString();
-    int celsiusValue = Integer.parseInt(celsius);
-    int fahrenheitConvert = celsiusValue * 9 / 5 + 32;
-    fahr.setText(getString(R.string.int_place_holder, fahrenheitConvert));
+    double celsiusValue = Double.parseDouble(celsius);
+    double fahrenheitConvert = celsiusValue * 9 / 5 + 32;
+    fahr.setText(getString(R.string.double_place_holder, fahrenheitConvert));
     double kelvinConvert = celsiusValue + 273.15;
     kelv.setText(getString(R.string.double_place_holder, kelvinConvert));
   }
@@ -73,10 +66,10 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
   private void setKelv() {
     String kelvin = kelv.getText().toString();
     double kelvinValue = Double.parseDouble(kelvin);
-    int fahrenheitConvert = (int) (Math.round(kelvinValue * 9 / 5 - 459.67));
-    fahr.setText(fahrenheitConvert);
-    int celsiusConvert = (int) Math.round(kelvinValue - 273.15);
-    cels.setText(getString(R.string.int_place_holder, celsiusConvert));
+    double fahrenheitConvert = kelvinValue * 9 / 5 - 459.67;
+    fahr.setText(getString(R.string.double_place_holder, fahrenheitConvert));
+    double celsiusConvert = kelvinValue - 273.15;
+    cels.setText(getString(R.string.double_place_holder, celsiusConvert));
   }
 
 
